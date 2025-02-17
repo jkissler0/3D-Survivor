0596174f97e7bb5454bf10d03050e1b21f9a1244
Jake Kissler <jakekissler@gmail.com>
2025-02-16 20:11:39 -0500

    Basic Player Health
    Added the following:
    - New Canvas object with a TextMeshPro child that displays the Player's current health (100 max) in green text on the bottom of the screen
    - A new Enemy tag that has been applied to all enemy objects
    - Modifications to the PlayerController script that initialize a public health variable, adds a function to set the health text to the player's current health each frame and say "You Lose!" when it reaches zero, and another function that checks collisions every frame to see if any of them have the "Enemy" tag and if so, it lowers player health.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using UnityEditor.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
+    public TextMeshProUGUI healthText;
     // private int count;
     private Vector2 moveInput;
     private Vector3 movement;
     public float speed = 0;
+    public float health = 100;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,11 +25,33 @@ public class PlayerController : MonoBehaviour
         moveInput = movementValue.Get<Vector2>();
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+                health -= (50 * Time.fixedDeltaTime);
+        }
+    }
+
+    void SetHealth()
+    {
+        int healthInt = Mathf.RoundToInt(health);
+        healthText.text = healthInt.ToString();
+        if (health <= 0)
+        {
+            healthText.color = Color.black;
+            healthText.text = "You Lose!";
+            this.gameObject.SetActive(false);
+        }
+    }
 
     private void FixedUpdate()
     {
         movement.x = moveInput.x * speed;
         movement.z = moveInput.y * speed;
         controller.Move(movement * Time.fixedDeltaTime);
+        
+        SetHealth();
+
     }
 }
