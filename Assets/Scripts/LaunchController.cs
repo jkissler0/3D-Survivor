1392c2b071f08f646b507e22c88413892ab8d071
Markus-Walker02 <walkermarkus22@gmail.com>
2025-02-17 14:31:59 -0500

    Update LaunchController.cs

--- Assets/Scripts/LaunchController.cs
+++ Assets/Scripts/LaunchController.cs
@@ -1,1 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehavior {
+  public GameObject projectile;
+  public float launchVelocity = 700f;
+
+  void Update() {
+
+    if (Input.GetButtonDown("Fire1")) {
+
+      GameObject bullet = Instantiate (projectile, transform.position, transform.rotation);
+
+      bullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 0, launchVelocity, 0);
+
+    }
+    
+  }
+
+}
+
 
