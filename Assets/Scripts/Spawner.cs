c4231de9425c7cfc003ab014d935b7d3a1cbcd89
Jake Kissler <jakekissler@gmail.com>
2025-02-22 18:11:16 -0500

    Enemy Spawn System
    Added a new empty enemies object to hold all enemy objects with a script that spawns an enemy every 5 seconds, the time can be customized and even randomized later on if we want. Spawned enemies will have a 75/25 ratio of basic enemies to fast enemies. This function should still work with any level as long as there is a NavMesh, we will just need to change the radius enemies spawn in for bigger levels.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,26 +1,48 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Spawner : MonoBehaviour
 {
     public GameObject basicEnemy;
     public GameObject fastEnemy;
-    public GameObject level;
-    public Transform player;
     private double timer;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Vector3 RandomNavmeshLocation(float radius)
     {
-        var sizeX = level.GetComponent<Renderer>().bounds.size.x;
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        while (finalPosition.y > 2)
+        {
+            finalPosition = RandomNavmeshLocation(radius);
+        }
+        return finalPosition;
     }
 
     void FixedUpdate()
     {
-        if (timer < 50) {
+        if (timer < 5) {
             timer += 0.02;
         } else {
             // Spawn enemy
-            timer = 0;
+            float randSpawn = Random.value;
+            if (randSpawn < 0.75)
+            {
+                Vector3 spawnPos = RandomNavmeshLocation(95);
+                Instantiate(basicEnemy, spawnPos, Quaternion.identity, this.gameObject.transform);
+                timer = 0;
+            } else
+            {
+                Vector3 spawnPos = RandomNavmeshLocation(95);
+                Instantiate(fastEnemy, spawnPos, Quaternion.identity, this.gameObject.transform);
+                timer = 0;
+            }
+            
         }
     }
 }
