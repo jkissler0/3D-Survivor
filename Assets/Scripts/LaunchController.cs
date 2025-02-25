7eb2d0f09db156cdc813021d9d63628338077e4c
Markus-Walker02 <walkermarkus22@gmail.com>
2025-02-25 14:45:18 -0500

    Add files via upload

---
+++ Assets/Scripts/LaunchController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LaunchProjectile : MonoBehaviour
+{
+  public GameObject projectile;
+  public float launchVelocity = 700f;
+
+  void Update()
+  {
+
+    if (Input.GetButtonDown("Fire1"))
+    {
+
+      GameObject bullet = Instantiate(projectile, transform.position, transform.rotation);
+
+      bullet.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, launchVelocity));
+
+    }
+  }
+
+  void
+}
+
+}
+
+
